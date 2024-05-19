9876a971faf771405e77d41f2bd2aa6f92efd7b7
tabish-siraj <tabish1226@gmail.com>
2024-05-19 22:08:53 +0500

    deploying

--- project_management/settings.py
+++ project_management/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,8 +27,7 @@ SECRET_KEY = 'django-insecure-&h^)q&8oavuxt-&jgw#g9zxqa77!o5@u%*(-=+hmn*$vdyik55
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
 
 # Application definition
 
